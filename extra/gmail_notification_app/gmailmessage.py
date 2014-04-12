dade469321b20df817781abb06b7b32ea04c7813
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-04-12 23:03:33 +0530

    Added Previous Message variable in config.ini
      1. This Variable stores the number of unread messages.
      2. If current number of unread message is equal to the previousmessage
    variable, the notification popup is disabled.

--- extra/gmail_notification_app/gmailmessage.py
+++ extra/gmail_notification_app/gmailmessage.py
@@ -1,14 +1,21 @@
-import urllib2
-import subprocess
-from bs4 import BeautifulSoup
-import time
-import ConfigParser
+#!/usr/bin/env python
+
 import os
 import sys
+import time
+import urllib2
+import subprocess
+import ConfigParser
+from bs4 import BeautifulSoup
+
 
 FEED_URL = 'https://mail.google.com/mail/feed/atom'
+
+
 cwd = sys.path[0]
 basefile = os.path.join(cwd, 'config.ini')
+
+
 Config = ConfigParser.ConfigParser()
 Config.read(basefile)
 
@@ -37,10 +44,10 @@ def internet_on():
 
 class Gmailnotification:
 
-	def __init__(self, user, passwd):
-		self.getnumberofmessage(user, passwd)
+	def __init__(self, user, passwd, previousnumber):
+		self.getnumberofmessage(user, passwd, previousnumber)
 
-	def getnumberofmessage(self, user, passwd):
+	def getnumberofmessage(self, user, passwd, previousnumber):
 		auth_handler = urllib2.HTTPBasicAuthHandler()
 		auth_handler.add_password(
 			realm = 'New mail feed',
@@ -50,33 +57,46 @@ class Gmailnotification:
 		)
 		opener = urllib2.build_opener(auth_handler)
 		urllib2.install_opener(opener)
-		feed = urllib2.urlopen(FEED_URL)
+		try:
+			feed = urllib2.urlopen(FEED_URL)
+			self.parsingfullcount(feed, previousnumber)
+		except:
+			return
+
+	def parsingfullcount(self, feed, previousnumber):
 		soup = BeautifulSoup(feed.read())
 		number = soup.fullcount.string
 		number = int(number)
 		unreadmessages = "You have %d unread mails in your gmail inbox" % int(number)
-		self.sendmessage(unreadmessages, number)
+		self.sendmessage(unreadmessages, number, previousnumber)
 
-	def sendmessage(self, message, number):
+	def sendmessage(self, message, number, previousnumber):
 		nomessage = "No unread mails in your gmail inbox"
-		self.previousnumber = 0
-		if number == self.previousnumber:
+		if number == int(previousnumber):
+			return
+		else:
 			if number == 0:
 				subprocess.Popen(['notify-send', nomessage])
-				previousnumber = number
 			else:
-				return number
-		else:
-			subprocess.Popen(['notify-send', message])
-			previousnumber = number
+				subprocess.Popen(['notify-send', message])
+				self.updateconfig(number)
 
+	def updateconfig(self, number):
+		self.cwd = sys.path[0]
+		self.basefile = os.path.join(self.cwd, 'config.ini')
+		self.editconfig = ConfigParser.RawConfigParser()
+		self.editconfig.read(self.basefile)
+		self.editconfig.set('SectionOne', 'previousnumber', number)
+		with open(self.basefile, 'wb') as configfile:
+			self.editconfig.write(configfile)
 
 if __name__ == "__main__":
 	while True:
 		if internet_on():
 			user = ConfigSectionMap("SectionOne")['username']
 			passwd = ConfigSectionMap("SectionOne")['password']
-			d = Gmailnotification(user, passwd)
+			previousnumber = ConfigSectionMap("SectionOne")['previousnumber']
+			d = Gmailnotification(user, passwd, previousnumber)
 			time.sleep(300)
 		else:
 			time.sleep(30)
